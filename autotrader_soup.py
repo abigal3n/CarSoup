ce821fae857872512617369ca61336ac55c5fba1
abigal3n <abbykittygaming13@gmail.com>
2024-10-01 17:31:30 -0400

    now finds mileage

--- autotrader_soup.py
+++ autotrader_soup.py
@@ -7,14 +7,16 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="srp-listings")
 cars = results.find_all("div", class_="col-xs-12 col-sm-4 display-flex")
+print(len(cars))
 
 for car in cars:
     car_title = car.find("h3", class_="text-bold text-size-400 link-unstyled")
     car_price = car.find("div", class_="text-size-600 text-ultra-bold first-price")
-    #car_mileage =
+    car_mileage = car.find("div", class_="text-bold text-subdued-lighter margin-top-3")
     
     #print(car, end="\n"*2)
     print(car_title)
     print(car_price)
+    print(car_mileage)
     print()
 #print(results.prettify)
